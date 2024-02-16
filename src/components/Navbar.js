e77120a70aa82d7b2bcc70d0e089bac348cf86c0
Snehesh Thattil <sneheshthattil75@gmail.com>
2024-02-16 17:03:53 +0530

    [Navbar completed; require improvements later]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import Navbar_Page from '../pages/Navbar_Page'
-
+import './Navbar.css'
 function Navbar() {
     return (
         <div>
-            <Navbar_Page></Navbar_Page>
+            <nav>
+                <a href="/"><img src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=740&t=st=1708069914~exp=1708070514~hmac=72878e423057afd425ac58838077f8a63a3f2d3d251abf76ba948668e5b5fb3d" alt="err" /></a>
+                <input type="text" placeholder='Food Item, Resturant, Location, etc' />
+
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/Wishlist">Wishlist</a></li>
+                    <li><a href="/Orders">Orders</a></li>
+                    <li><a href="/Resturants">Resturants</a></li>
+                </ul>
+                <div className="dropdown">
+                    <button className='profile-dp'>Profile</button>
+                    <div className="dp-items">
+                        <a href="/orders">Orders</a>
+                        <a href="/Location">Location</a>
+                        <a href="/Address">Address</a>
+                        <a href="/Payment">Payment</a>
+                    </div>
+                </div>
+
+            </nav>
         </div>
     )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Home() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Assume : Body</h1>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default Home
