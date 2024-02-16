9887e93bba47e6ef9d9bf690ac1fb7f3b8e89e5b
Snehesh Thattil <sneheshthattil75@gmail.com>
2024-02-16 12:31:53 +0530

    [ Initial Git Commit Safeguard ]

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar_Page from '../pages/Navbar_Page'
+
+function Navbar() {
+    return (
+        <div>
+            <Navbar_Page></Navbar_Page>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Navbar_Page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar_Page() {
+    return (
+        <div>
+            <h1>NavBar Page</h1>
+        </div>
+    )
+}
+
+export default Navbar_Page
